524f8f45b8b2a6e4b08e8aed2c3d90ec48119b09
mnewsome <malcolm.newsome@gmail.com>
2014-12-05 14:35:01 -0600

    adds user field to admin updates list filter in admin so now sortable by user and is_locked

--- thoughts/admin.py
+++ thoughts/admin.py
@@ -3,10 +3,10 @@ from thoughts.models import Thought
 
 class ThoughtAdmin(admin.ModelAdmin):
   fieldsets = [
-      ('Detail', {'fields': ['text']}),
+      ('Detail', {'fields': ['text', 'user']}),
       ('Status', {'fields': ['is_locked']}),
   ]
 
-  list_filter = ('date_created',)
+  list_filter = ('date_created', 'user', 'is_locked')
 
 admin.site.register(Thought, ThoughtAdmin)
