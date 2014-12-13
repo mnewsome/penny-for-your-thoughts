36ed1481551e6c5f8c30776325c3be1ab2e56089
mnewsome <malcolm.newsome@gmail.com>
2014-12-13 11:51:49 -0600

    adds logging

--- thoughts/tasks.py
+++ thoughts/tasks.py
@@ -1,9 +1,14 @@
 from __future__ import absolute_import
 
 from celery import shared_task
+from celery.utils.log import get_task_logger
 
 from thoughts.models import Thought
 
+logger = get_task_logger(__name__)
+
 @shared_task
 def unlock_thoughts(pool_size):
-  return Thought.unlock_thoughts(pool_size)
+  thoughts_unlocked = Thought.unlock_thoughts(pool_size)
+  logger.info("{0} thoughts unlocked".format(thoughts_unlocked))
+  return thoughts_unlocked
