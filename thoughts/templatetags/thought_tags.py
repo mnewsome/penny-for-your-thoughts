00bc183938aa67506ebd698c90e2ec9142fa65a1
mnewsome <malcolm.newsome@gmail.com>
2014-12-14 09:36:31 -0600

    updates show_thoughts inclusion tag  to display only unlocked thought

--- thoughts/templatetags/thought_tags.py
+++ thoughts/templatetags/thought_tags.py
@@ -5,6 +5,6 @@ from thoughts.models import Thought
 register = template.Library()
 
 @register.inclusion_tag('thoughts/index.html')
-def show_thoughts():
-  thoughts = Thought.objects.all().order_by('date_created')
+def show_unlocked_thoughts():
+  thoughts = Thought.objects.filter(is_locked=False).order_by('date_created')
   return {'thoughts': thoughts}
