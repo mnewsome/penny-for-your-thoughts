20042dbd238a7021928be4824bc9446c5cf192c4
mnewsome <malcolm.newsome@gmail.com>
2014-12-20 16:34:13 -0600

    spike for integrating stripe-very basic charge working

--- penny_for_your_thoughts/views.py
+++ penny_for_your_thoughts/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from os import environ as env
+from django.shortcuts import render, redirect
 
 from thoughts.forms import ThoughtForm
 from thoughts.models import Thought
+
+import stripe
+
+stripe_keys = dict(
+    secret_key=env.get('STRIPE_TEST_SECRET_KEY'),
+    publishable_key=env.get('STRIPE_TEST_PUB_KEY')
+    )
+
+stripe.api_key = stripe_keys['secret_key']
 
 def index(request):
   context = dict(
@@ -9,9 +19,28 @@ def index(request):
            unlocked_thought_count=Thought.unlocked_thought_count(),
            next_locked_thought=Thought.next_locked_thought(),
            thought_form=get_thought_form(request),
+           key=stripe_keys['publishable_key'],
            )
 
   return render(request, 'index.html', context)
+
+def charge(request):
+  if request.method == 'POST':
+    amount = 500
+
+    customer = stripe.Customer.create(
+        email='customer@example.com',
+        card=request.POST['stripeToken']
+        )
+
+    charge = stripe.Charge.create(
+        customer=customer.id,
+        amount=amount,
+        currency='usd',
+        description='Flask Charge'
+        )
+
+  return redirect('penny_for_your_thoughts.views.index')
 
 def get_thought_form(request):
   if request.method == 'POST':
