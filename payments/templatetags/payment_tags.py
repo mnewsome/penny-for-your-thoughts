adaf73eddf761269ffe3a8abb851fffc52631109
mnewsome <malcolm.newsome@gmail.com>
2015-01-17 23:42:51 -0600

    refactors register filter to use decorator

--- payments/templatetags/payment_tags.py
+++ payments/templatetags/payment_tags.py
@@ -2,7 +2,6 @@ from django import template
 
 register = template.Library()
 
+@register.filter(name='currency')
 def currency(value):
   return '${0}'.format(value)
-
-register.filter('currency', currency)
