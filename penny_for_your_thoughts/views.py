7d73e4bb7e82a91e632fc5f3fc7fe056bec10471
mnewsome <malcolm.newsome@gmail.com>
2015-05-30 14:54:23 -0500

    adds login/logout to view

--- penny_for_your_thoughts/views.py
+++ penny_for_your_thoughts/views.py
@@ -1,5 +1,8 @@
+from django.contrib               import messages
+from django.contrib.auth          import authenticate, login, logout
+from django.contrib.auth.forms    import AuthenticationForm
 from django.shortcuts             import render, redirect
-from django.views.decorators.http import require_POST
+from django.views.decorators.http import require_POST, require_GET
 
 from lib.payment_manager          import stripe_keys, PaymentManager
 from nosql_backend                import RedisWrapper
@@ -10,51 +13,78 @@ from payments.models              import Payment
 
 redis_store = RedisWrapper()
 
+@require_POST
+def login_user(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+
+    user = authenticate(username=username, password=password)
+
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            messages.success(request, 'Successfully logged in')
+            return redirect('penny_for_your_thoughts.views.index')
+        else:
+            messages.error(request, 'This account is not active')
+            return redirect('penny_for_your_thoughts.views.index')
+    else:
+        messages.error(request, 'Invalid username/password')
+        return redirect('penny_for_your_thoughts.views.index')
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'Logged out')
+    return redirect('penny_for_your_thoughts.views.index')
+
 def index(request):
-  if request.method == 'GET':
-    thought_form = ThoughtForm(initial={'user': request.user})
+    auth_form = AuthenticationForm(request)
 
-  if request.method == 'POST':
-    thought_form = ThoughtForm(request.POST)
+    if request.method == 'GET':
+        thought_form = ThoughtForm(initial={'user': request.user})
+
+    if request.method == 'POST':
+        thought_form = ThoughtForm(request.POST)
     if thought_form.is_valid():
-      thought_form.save()
-      redis_store.decrement_unlocked_thought_pool(1)
+        thought_form.save()
+        redis_store.decrement_unlocked_thought_pool(1)
 
-  context = dict(
-           locked_thought_count   = Thought.locked_thought_count(),
-           unlocked_thought_count = Thought.unlocked_thought_count(),
-           next_locked_thought    = Thought.next_locked_thought(),
-           thought_form           = thought_form,
-           total_dollars_donated  = Payment.total_dollars_donated(),
-           key                    = stripe_keys['publishable_key'],
-           )
+    context = dict(
+            auth_form              = auth_form,
+            locked_thought_count   = Thought.locked_thought_count(),
+            unlocked_thought_count = Thought.unlocked_thought_count(),
+            next_locked_thought    = Thought.next_locked_thought(),
+            thought_form           = thought_form,
+            total_dollars_donated  = Payment.total_dollars_donated(),
+            key                    = stripe_keys['publishable_key'],
+            )
 
-  Thought.unlock_thoughts(redis_store.unlocked_thought_pool_value())
-  return render(request, 'index.html', context)
+    Thought.unlock_thoughts(redis_store.unlocked_thought_pool_value())
+    return render(request, 'index.html', context)
 
 @require_POST
 def charge(request):
-  if request.method == 'POST':
-    amount = int(request.POST['amount'])
-    email = request.POST['email']
-    token = request.POST['stripeToken']
+    if request.method == 'POST':
+        amount = int(request.POST['amount'])
+        email = request.POST['email']
+        token = request.POST['stripeToken']
 
-    payment_manager = get_payment_manager()
-    customer = payment_manager.create_customer(email, token)
-    payment_manager.create_charge(customer, amount)
+        payment_manager = get_payment_manager()
+        customer = payment_manager.create_customer(email, token)
+        payment_manager.create_charge(customer, amount)
 
-    Payment(user=request.user, stripe_customer_id=customer.id, amount=amount).save()
-    updated_thought_count = Thought.unlock_thoughts(amount)
-    adjust_thought_pool(updated_thought_count, amount)
+        Payment(user=request.user, stripe_customer_id=customer.id, amount=amount).save()
+        updated_thought_count = Thought.unlock_thoughts(amount)
+        adjust_thought_pool(updated_thought_count, amount)
 
-  return redirect('penny_for_your_thoughts.views.index')
+    return redirect('penny_for_your_thoughts.views.index')
 
 def unlock_thoughts():
-  Thought.unlock_thoughts()
+    Thought.unlock_thoughts()
 
 def get_payment_manager():
-  return PaymentManager()
+    return PaymentManager()
 
 def adjust_thought_pool(thoughts_updated_count, payment_amount):
-  if thoughts_updated_count < payment_amount:
-    return redis_store.increment_unlocked_thought_pool(payment_amount - thoughts_updated_count)
+    if thoughts_updated_count < payment_amount:
+        return redis_store.increment_unlocked_thought_pool(payment_amount - thoughts_updated_count)
