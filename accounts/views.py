72b291c1d13cd1b288044b878960b2ea819b7da8
mnewsome <malcolm.newsome@gmail.com>
2015-06-26 18:02:48 -0500

    adds user's thoughts to their profile

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts         import render
-
+from django.shortcuts               import render
 from django.contrib.auth.models     import User
 from django.contrib.auth.decorators import login_required
+
+from thoughts.models import Thought
 
 @login_required
 def index(request):
 	user = User.objects.get(username=request.user.username)
+	thoughts = Thought.objects.filter(user=user)
 	context = dict(
-		general_user = user.generaluser
+		general_user = user.generaluser,
+		thoughts     = thoughts,
 	)
 	return render(request, 'accounts/index.html', context)
